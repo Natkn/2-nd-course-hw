113b452db0e692279fb210928007af324527a88b
Nastya Star <harryisokay@gmail.com>
2024-09-05 18:53:00 +0300

    EDITED NAMES

--- js/java.js
+++ js/java.js
@@ -1,20 +1,20 @@
-
+//РЕШЕНИЕ ЗАДАЧИ 1
 let a = Number(20);
 alert(a); 
 
 
-
-let Year= ("2007");
-alert( `Год выпуска первого iPhone- ${Year}`); 
-
-
-
-let  Surname= ('Brendon Eich');
-alert(`Создатель языка JavaScript- ${Surname}`);
+//РЕШЕНИЕ ЗАДАЧИ 2
+let year= ("2007");
+alert( `Год выпуска первого iPhone- ${year}`); 
 
 
+//РЕШЕНИЕ ЗАДАЧИ 3
+let  surname= ('Brendon Eich');
+alert(`Создатель языка JavaScript- ${surname}`);
 
 
+
+//РЕШЕНИЕ ЗАДАЧИ 4
 let a = Number(10);
 let b =Number(2);
 let sum= a+b;
@@ -27,14 +27,14 @@ alert(`Сумма переменных ${sum},
 Частное переменных ${quotient}.`);
 
 
-
+//РЕШЕНИЕ ЗАДАЧИ 5
 let a = Number(2);
 let power = a**5;
 alert(power);
 
 
 
-
+//РЕШЕНИЕ ЗАДАЧИ 6
 let a= Number(9);
 let b= Number(2);
 let remainder= a%b;
@@ -43,7 +43,7 @@ alert(remainder);
 
 
 
-
+//РЕШЕНИЕ ЗАДАЧИ 7
 let num = 1;
 num += 5;
 num -= 3;
@@ -55,19 +55,19 @@ alert(num);
 
 
 
-
+//РЕШЕНИЕ ЗАДАЧИ 8
 let age = Number(prompt( "Сколько вам лет?"));
 alert( ` Вам ${age}`);  
 
 
 
-
+//РЕШЕНИЕ ЗАДАЧИ 9
 const user ={
     name:"Willy",
     age:3,
 isAdmin: true};
 
 
-
+//РЕШЕНИЕ ЗАДАЧИ 10
 let username = prompt("Введите ваше имя:");
 alert(`Привет, ${username}!`);
