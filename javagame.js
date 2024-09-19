3a29490725ab9958306daf9278c4a2b9ced61f26
Nastya Star <harryisokay@gmail.com>
2024-09-19 19:15:44 +0300

    changes

--- javagame.js
+++ javagame.js
@@ -1,11 +1,15 @@
 
 function showTextGame() {
+let answer = 73;
+while (true) {
     let userAnswer = prompt('Угадайте число от 1 до 100');
-    if (userAnswer == 73) {
+   if (userAnswer == answer) {
         alert('Ты угадал!');
-    } else if (userAnswer < 73) {
+        break;
+    }  else if (userAnswer < answer) {
         alert('Не угадал, больше.');
     } else {
         alert('Не угадал, меньше.');
     }
+}
 }
