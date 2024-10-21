7fe99063a94f0d559b2755700209e41bbf558aa7
Nastya Star <103874674+Natkn@users.noreply.github.com>
2024-10-21 15:07:47 +0300

    final changes

--- javagame.js
+++ javagame.js
@@ -1,22 +1,19 @@
-
-
 function showTextGame() {
-let answer = 73;
-while (true) {
-    let userAnswer = prompt('Угадайте число от 1 до 100');
-   if (userAnswer == answer) {
-        alert('Ты угадал!');
+   
+    let answer = Math.floor(Math.random() * 100) + 1;
+    
+  
+    while (true) {
+      let userAnswer = prompt('Угадайте число от 1 до 100');
+     
+  
+      if (userAnswer == answer) {
+        alert(`Ты угадал!`); 
         break;
-    }  else if (userAnswer < answer) {
+      } else if (userAnswer < answer) {
         alert('Не угадал, больше.');
-    } else {
+      } else {
         alert('Не угадал, меньше.');
+      }
     }
-}
-}
-
-
-
-
-
-
+  }
